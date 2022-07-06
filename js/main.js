ae5f55ed25c86223e0e113988372ae41e6df03a4
pacimarco <m.paci1987@gmail.com>
2022-07-06 15:38:49 +0200

    push prova colori multipli

--- js/main.js
+++ js/main.js
@@ -6,19 +6,23 @@ for (let i=0; i<=100; i++) {
     div.classList.add('box');
     containerDom.appendChild(div);
     console.log(i);
-    containerDom.innerHTML += '<div class="box"> i </div>';
+   // containerDom.innerHTML += '<div class="box"> i </div>';//
 
     if (i % 3 ==0) {
         div.classList.add('box-red');
         containerDom.innerHTML += '<div class="box"> Fizz </div>';
+        console.log('Fizz');
     
     }else if (i % 5 ==0) {
         div.classList.add('box-blue');
         containerDom.innerHTML += '<div class="box"> Buzz </div>';
+        console.log('Buzz');
+
     
     }else if (i % 3 ==0 && i % 5 ==0) {
         div.classList.add('box-green');
         div.innerHTML += '<div class="box"> FizzBuzz </div>';
+        console.log('FizzBuzz');
     
     }
 
